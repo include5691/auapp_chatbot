64921a8efcccc3de9df0c9906f2959c521c1ac60
Andrey Pshenitsyn <include5691@gmail.com>
2025-01-10 15:48:32 +0500

    update[bot]: update logging and add blocked error handler

--- bot.py
+++ bot.py
@@ -2,8 +2,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 import logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
-
 import os
 import logging
 import asyncio
@@ -14,11 +12,15 @@ from colorama import Fore, Style
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
 from aiogram.types import Message
+from aiogram.exceptions import TelegramForbiddenError
 from au_b24 import get_leads, update_lead, get_lead, get_user
 from e5lib.funcs import phone_purge, create_phone_vars
 from e5lib.time import get_yesterday
 from aulib.au_engine import get_engines
 from _redis import redis_cli
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
+logging.getLogger('aiogram').setLevel(logging.WARNING)
 
 dp = Dispatcher()
 bot = Bot(token=os.getenv("TELEGRAM_BOT_API_TOKEN"))
@@ -29,12 +31,15 @@ async def begin_handler(message: Message) -> None:
 
 @dp.message()
 async def phone_validation_handler(message: Message) -> None:
+    logging.info(message.text)
     phone = phone_purge(message.text)
     if not phone:
+        logging.info(f"{Fore.RED}Invalid phone number{Style.RESET_ALL}")
         await message.answer(os.getenv("PHONE_INVALID_TEXT"))
         return
     phone_vars = create_phone_vars(phone, "*")
     if not phone_vars:
+        logging.info(f"{Fore.RED}Invalid phone number{Style.RESET_ALL}")
         await message.answer(os.getenv("PHONE_PARSE_INVALID_TEXT"))
         return
     lead = None
@@ -44,6 +49,7 @@ async def phone_validation_handler(message: Message) -> None:
             lead = leads[0]
             break
     if not lead:
+        logging.info(f"{Fore.RED}Lead not found{Style.RESET_ALL}")
         await message.answer(os.getenv("LEAD_NOT_FOUND_TEXT"))
         return
     lead_id = lead.get("ID")
@@ -80,8 +86,11 @@ async def process_distributed_lead(request: Request):
     if not engines:
         return
     wa_link = os.getenv("WA_API_URL").format(phone=phone_purge(engines[0].wid), text=quote(os.getenv("WA_FIRST_MESSAGE_TEXT")))
-    await bot.send_message(chat_id.decode(), os.getenv("LEAD_DIST_TEXT").format(user.get("NAME"), wa_link))
-    logging.info(f"{Fore.LIGHTGREEN_EX}Sent dist text to {lead_id}{Style.RESET_ALL}")
+    try:
+        await bot.send_message(chat_id.decode(), os.getenv("LEAD_DIST_TEXT").format(user.get("NAME"), wa_link))
+        logging.info(f"{Fore.LIGHTGREEN_EX}Sent dist text to {lead_id}{Style.RESET_ALL}")
+    except TelegramForbiddenError as e:
+        logging.error(f"{Fore.RED}TelegramForbiddenError: {e}{Style.RESET_ALL}")
 
 async def main():
     config = uvicorn.Config(app, host="0.0.0.0", port=8500, log_level="warning", loop="asyncio")
